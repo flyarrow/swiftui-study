839fd192adf7f2ff8acd3dfaec2911a5ee270c35
John Chen <john.cxh@gmail.com>
2024-11-13 11:29:48 +0800

    Initial Commit

---
+++ swiftUI_learning/swiftUI_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_learningApp.swift
+//  swiftUI_learning
+//
+//  Created by XIAO HAN CHEN on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swiftUI_learningTests/swiftUI_learningTests.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_learningTests.swift
+//  swiftUI_learningTests
+//
+//  Created by XIAO HAN CHEN on 2024/11/13.
+//
+
+import Testing
+@testable import swiftUI_learning
+
+struct swiftUI_learningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
